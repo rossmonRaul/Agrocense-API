03d7832fd3626e6d8c3fd0df7927195464ed12d8
Kendall Madrigal <kendallmadrigal14@gmail.com>
2024-03-15 15:55:13 -0600

    Agregando el Dto y el Entity de RotacionCultivos

---
+++ Agrosense.Service.Model/Entity/EntityRotacionCultivos.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Agrosense.Service.Model.Entity
+{
+    public class EntityRotacionCultivos
+    {
+        public int? IdRotacionCultivoSegunEstacionalidad { get; set; }
+        public int IdFinca { get; set; }
+        public int IdParcela { get; set; }
+        public string? Cultivo { get; set; }
+        public string? EpocaSiembra { get; set; }
+        public string? TiempoCosecha { get; set; }
+        public string? CultivoSiguiente { get; set; }
+        public string? EpocaSiembraCultivoSiguiente { get; set; }
+        public int Estado { get; set; }
+    }
+}
